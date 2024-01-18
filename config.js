dbf9638d7736d51e5f51638513942a794c73b12c
VladimirZimin <zimin.frontend@gmail.com>
2024-01-18 23:24:42 +0200

    add DB_HOST

---
+++ config.js
@@ -0,0 +1,6 @@
+const DB_HOST =
+  "mongodb+srv://Admin:qweqwe@cluster0.lkce23l.mongodb.net/books_reader?retryWrites=true&w=majority";
+
+module.exports = {
+  DB_HOST,
+};
--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Admin:qweqwe@cluster0.lkce23l.mongodb.net/books_reader?retryWrites=true&w=majority";
+const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
 
